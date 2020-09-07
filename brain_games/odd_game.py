119f7798d82ab5e152aa2d636a6da48ed11939fb
liuxindao <jokerbell7@gmail.com>
2020-09-07 18:26:36 +0300

    update odd game files

--- brain_games/odd_game.py
+++ brain_games/odd_game.py
@@ -2,32 +2,35 @@
 
 """Check if number is odd."""
 
-import prompt, random
+import random
+
+import prompt
 
 
 def odd_check(answer, number, name):
+    """Check if number is odd."""
     if number % 2 == 0:
         if answer == 'yes':
             return 'Correct!'
-        else:
-            return "'{ans}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!".format(ans=answer, name=name)
-    else:
-        if answer == 'no':
-            return 'Correct!'
-        else:
-            return "'{ans}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!".format(ans=answer, name=name)       
+        return "'{ans}' is wrong answer ;(. Correct answer was\
+         'yes'.\nLet's try again, {name}!".format(ans=answer, name=name)
+    if answer == 'no':
+        return 'Correct!'
+    return "'{ans}' is wrong answer ;(. Correct answer was\
+         'no'.\nLet's try again, {name}!".format(ans=answer, name=name) 
 
 
 def game():
+    """Play a game."""
     name = prompt.string('\nMay I have your name? ')
     print('Hello, {name}!\n'.format(name=name))
-    for i in range(3):
-        number = random.randint(0,1000)
+    for count in range(3):
+        number = random.randint(0, 1000)
         print('Question: ', number)
         answer = prompt.string('Your answer: ')
-        result = odd_check(answer, number, name)
-        if result != 'Correct!':
-            print(result)
+        output = odd_check(answer, number, name)
+        if output != 'Correct!':
+            print(output)
             break
-        if i == 2:
-            print('Congratulations, {name}!'.format(name=name))     	    
+        if count == 2:
+            print('Congratulations, {name}!'.format(name=name)) 
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,14 +1,16 @@
 # -*- coding:utf-8 -*-
 
 """Odd game."""
-    
+
 from brain_games import odd_game
 
 
 def main():
-    print('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".')
+    """Starting odd game."""
+    print('Welcome to the Brain Games!\nAnswer\
+     "yes" if number even otherwise answer "no".')
     odd_game.game()
 
-    
+
 if __name__ == '__main__':
-    main()       	    
+    main()
